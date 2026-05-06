3a473fcf6301f2666292f5bfca864afefb3be2c0
! Meisther <fabianyneron@gmail.com>
2026-05-06 16:32:40 -0500

    fix(compute): actualizar variables del modulo compute

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,9 +1,34 @@
-variable "environment" { type = string }
-variable "bucket_id" { type = string }
-variable "bucket_arn" { type = string }
-variable "sqs_main_queue_arn" { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "sg_upload_lambda_id" { type = string }
-variable "sg_crop_lambda_id" { type = string }
-variable "lambda_upload_memory" { type = number; default = 256 }
-variable "lambda_crop_memory" { type = number; default = 512 }
+variable "environment" {
+  type        = string
+  description = "Entorno de despliegue"
+}
+
+variable "bucket_id" {
+  type        = string
+  description = "ID del bucket de S3"
+}
+
+variable "bucket_arn" {
+  type        = string
+  description = "ARN del bucket de S3"
+}
+
+variable "sqs_main_queue_arn" {
+  type        = string
+  description = "ARN de la cola SQS"
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "Lista de IDs de subredes privadas"
+}
+
+variable "sg_upload_lambda_id" {
+  type        = string
+  description = "Security Group para la Lambda de subida"
+}
+
+variable "sg_crop_lambda_id" {
+  type        = string
+  description = "Security Group para la Lambda de recorte"
+}
