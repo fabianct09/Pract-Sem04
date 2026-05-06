bb2aa2280cb909f11fa3d9b2a11f5e3c79508a34
! Meisther <fabianyneron@gmail.com>
2026-05-06 16:32:54 -0500

    fix(api-gateway): corregir output de URL base

--- modules/api_gateway/outputs.tf
+++ modules/api_gateway/outputs.tf
@@ -1,4 +1,4 @@
+# modules/api_gateway/outputs.tf
 output "base_url" {
-  description = "URL de la API"
-  value       = "${aws_api_gateway_stage.stage.invoke_url}/upload"
+  value = "${aws_api_gateway_stage.stage.invoke_url}/upload"
 }
